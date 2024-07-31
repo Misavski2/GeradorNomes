0e19d1e409f94e01b047cf0178e4d323e3eaa713
Misael Ferreira <misaelkaina2014@hotmail.com>
2024-07-31 16:59:10 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeradorNomes
+{
+    class Gerador
+    {
+        static void Main(string[] args)
+        {
+            List<string> start = new List<string> { "Dark", "Light", "Shadow", "Myth", "God", "Pro" };
+            List<string> end = new List<string> { "Zin", "vski", "God", "Ninja", "Knight", "Pro", "Master" };
+
+            Random random = new Random();
+
+            while (true)
+            {
+                Console.WriteLine("Pressione 'ENTER' para gerar um novo nick ou digite 'sair' para finalizar.");
+                string opcao = Console.ReadLine();
+
+                if (opcao.ToLower() == "sair")
+                {
+                    break;
+                }
+
+                //Gerar partes aleatorias
+                string stt = start[random.Next(start.Count)];
+                string ed = end[random.Next(end.Count)];
+
+                //Juntar 
+                string resultado = stt + ed;
+
+                Console.WriteLine("Seu novo nick é: " + resultado);
+            }
+        }
+    }
+}
